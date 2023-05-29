06deb91f2ee45eaf2f306aaab155d760f177c571
tenitaku <kukutaku23@gmail.com>
2023-05-29 12:56:41 +0900

    dictのhashableの概念難しい

---
+++ 49_GroupAnagrams_20230529.py
@@ -0,0 +1,35 @@
+"""
+given an array of strings strs, group the anagrams together.
+example....
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+output = [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
+"""
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        ans = defaultdict(list) #mapping charCount to list of Anagrams
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1  # ord can change char to unicode ("a" == 80)
+            
+            ans[tuple(count)].append(s)
+        
+        return ans.values()
+"""
+さすがに初見では難しかったので日本語で解説
+まず、最後に返すdictionaryを作成する、ここではlist型のdictionary
+次に、strsの文字を順に走査していく
+aからzまで、登場回数を数え、count[c]++していく
+最後に、登場回数が同じものの語句をansのvalueのlistに追加する。
+ここで気をつけるのは、ans[list(count)].append(s)としても動作しない点である。
+tuple(count)を使用する。
+理由として、dictionaryのkeyはhashableでなくてはならないからである。（hashableとは変更ができないこと）
+listは変更可能なので、かわりにtupleを使用する。
+"""
+
+sol = Solution()
+print(sol.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
